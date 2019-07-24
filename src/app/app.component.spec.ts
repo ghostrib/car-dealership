143a3e020a5a0d687b2d8aa9736f9ac7aafc6240
ghostrib <sh4rkbot@gmail.com>
2019-07-23 20:56:32 -0500

    update test file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'car-dealership'`, () => {
+  it(`should have as title 'Car Dealership'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('car-dealership');
+    expect(app.title).toEqual('Car Dealership');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title in a h3 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to car-dealership!');
+    expect(compiled.querySelector('h3').textContent).toContain('Welcome to Car Dealership');
   });
 });
